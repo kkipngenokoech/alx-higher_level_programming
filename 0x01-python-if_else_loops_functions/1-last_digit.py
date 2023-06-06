dcc3e7807a4a738fb6298dec122b1ba834b263ba
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-06 05:15:35 -0700

    it has to pass all checkers now

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,10 @@ number = random.randint(-10000, 10000)
 digit = abs(number) % 10
 if number >= 0:
     if digit == 0:
-        print(f'Last digit of {number} is {number % 10} and is 0')
+        print(f'Last digit of {number} is {digit} and is 0')
     elif digit >= 6:
-        print(f'Last digit of {number} is {number % 10} and is greater than 5')
+        print(f'Last digit of {number} is {digit} and is greater than 5')
     else:
-        print(f'Last digit of {number} is {number % 10} and is less than 6 and not 0')
+        print(f'Last digit of {number} is {digit} and is less than 6 and not 0')
 else:
-    number = abs(number)
-    print(f'Last digit of {number} is {number % 10} and is less than 6 and not 0')
+    print(f'Last digit of {number} is -{digit} and is less than 6 and not 0')
