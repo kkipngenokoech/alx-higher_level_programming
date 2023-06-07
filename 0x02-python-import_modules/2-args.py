2c4c6144eca4054563f0d8d84deb2d4b7233a4d1
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 01:44:12 -0700

    just because i did not include : my checker fails

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
     if count == 1:
-        print("{} argument".format(count))
+        print("{} argument:".format(count))
     elif count > 1:
-        print("{} arguments".format(count))
+        print("{} arguments:".format(count))
     else:
         print('.')
     for index, argument in enumerate(sys.argv):
