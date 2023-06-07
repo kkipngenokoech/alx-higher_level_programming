35b5a1f0a0c2bb59c2ddcdbc72af4427141fad33
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 03:56:34 -0700

    return none on no character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    firstA = sentence[0]
+    if length >= 1:
+        firstA = sentence[0]
+    else:
+        firstA = 'None'
     tupleReturn = (length, firstA)
     return tupleReturn
