bd4ad14412080b92146817cda3a66fc17b7020b1
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 01:38:46 -0700

    getting arguments from the terminal using sys.argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("{} argument".format(count))
+    elif count > 1:
+        print("{} arguments".format(count))
+    else:
+        print('.')
+    for index, argument in enumerate(sys.argv):
+        print("{}: {}".format(index, argument))
