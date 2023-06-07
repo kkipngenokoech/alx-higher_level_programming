9351669f8ab53da14c871bc6a334db36641f730c
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 03:02:12 -0700

    what to return

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!usr/bin/python3
 def element_at(my_list, idx):
     if idx > len(my_list) or idx < 0:
-        return 'None'
-    else:
-        return my_list[idx]
+        return 
+    return my_list[idx]
