c62d67e476983a3aaa48c934b29c0e434555179b
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 03:04:21 -0700

    refactor

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,8 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
+# that retrieves an element from a list like in C.
+
 def element_at(my_list, idx):
-    if idx > len(my_list) or idx < 0:
-        return 
+    if idx < 0 or idx >= len(my_list):
+        return
     return my_list[idx]
