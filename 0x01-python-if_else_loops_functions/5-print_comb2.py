9fcbae0a6bf0a6b6477b8f146164c316cd2bc9e6
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-06 23:08:35 -0700

    using format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for count in range(0,100):
     if count < 10:
         count = "0"+str(count)
     elif count == 99:
-        print(count)
+        print("{}".format(count))
         continue
-    print(count, ",", end="")
+    print("{}".format(count), ",", end="")
