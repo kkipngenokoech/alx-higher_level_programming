124bec0a293b743cdabd11814ab9e4e06d2dd521
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 00:09:51 -0700

    fixing the pycodesty;e

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,9 @@
 def uppercase(str):
     charString = list(str)
     for index, character in enumerate(charString):
-        if ord(character) in range(97,123):
+        if ord(character) in range(97, 123):
             ascii = ord(character) - 32
             character = chr(ascii)
             charString[index] = character
     sentence = "".join(charString)
     print("{}".format(sentence))
-
