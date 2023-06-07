767fd6c0c607e263f2dee07b93d5c7dd4a5a7f94
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-06 23:41:13 -0700

    the two digits must not be the same

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 for count in range(0, 10):
     for counter in range(count, 10):
-        if count == 9 and counter == 9:
+        if count == counter:
+            continue
+        if count == 8 and counter == 9:
             print("{}{}".format(count, counter))
             continue
         print("{}{}, ".format(count, counter), end="")
