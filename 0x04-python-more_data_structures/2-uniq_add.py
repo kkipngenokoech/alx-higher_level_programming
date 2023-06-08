d1bdc6238e42e3871ecbebb755132284e553ce69
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 22:45:44 -0700

    set()

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    my_list = list(set(my_list))
-
+def uniq_add(my_list[]):
     findsum = 0
-    for index in range(0, len(my_list)):
-            findsum += my_list[index]
+    for value in set(my_list):
+        findsum += value
     return findsum
