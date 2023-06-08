09570f661fd1900c3274d7ac0082f346fcb5e6b6
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 22:24:20 -0700

    set() to get unique characters in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    my_list = list(set(my_list))
+    findsum = 0
+    for index in range(0, len(my_list)):
+            findsum += my_list[index]
+    return findsum
