2c5c73e5062d874d7ed80ba480209025258e5282
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-08 00:37:13 -0700

    for the sake of the checkers

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary:
-        return dict(sorted(a_dictionary.items(), key=lambda item: item[1])).popitem()
+def best_score(my_dict):
+    if my_dict and len(my_dict):
+        max = list(my_dict.keys())[0]
+        for key in my_dict:
+            if my_dict[key] > my_dict[max]:
+                max = key
+        return max
+    return None
