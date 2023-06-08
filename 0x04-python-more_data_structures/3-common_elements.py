581c0a154e2d737cad104cc11bf170736b4b7949
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-07 22:42:51 -0700

    return set_1 and set_2

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    list1 = list(set_1)
-    list2 = list(set_2)
-    indices_present = [value for index, value in enumerate(list1) if value in list2]
-    set1 = set(indices_present)
-    return indices_present
+    return set_1 & set_2
