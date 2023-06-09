bfef7aa4e9e3ab77cff890fb25da5e48f63745f2
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-09 00:41:13 -0700

    index out of range

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    counter = 0
-    for index in range(x):
+    """Print the first x elements of a list that are integers.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[index]), end="")
-            counter += 1
-        except ValueError:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
             continue
-        except IndexError:
-            print("IndexError: list index out of range")
-        except:
-            pass
-    return counter
+    print("")
+    return (ret)
