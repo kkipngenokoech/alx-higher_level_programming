8d85431c9694e8ebc0352c0f0d17da266179fb3e
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-09 04:47:13 -0700

    test attrivutes

--- 0x06-python-classes/tests/1-main.py
+++ 0x06-python-classes/tests/1-main.py
@@ -1,3 +1,18 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 import sys
 sys.path.append('../')
+Square = __import__('1-square').Square
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
