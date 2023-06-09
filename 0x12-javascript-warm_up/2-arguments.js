b0209988331c3f1ec6121fdc55307c32546794dc
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-09 12:30:01 -0700

    else if

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length  === 2)
-{
+if (process.argv.length  === 2){
 	console.log('No argument');
 }
-else if (process.argv.length === 3)
-{
+else if (process.argv.length === 3){
 	console.log('Argument found');
 }
-else
-{
+else{
 	console.log('Arguments found');
 }
