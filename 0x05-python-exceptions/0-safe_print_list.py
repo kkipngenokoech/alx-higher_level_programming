e1bce3e723e76e54856c27209e2ebefff2e7b714
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-09 00:20:02 -0700

    forthe sake of checkers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    counter = 0
-    for index in range(0, x):
-        try:
-            print(my_list[index], end="")
-            counter += 1
-        except IndexError:
-            pass
-    print("")
-    return counter
 
+def safe_print_list(my_list=[], x=0):
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            return True
+        else
