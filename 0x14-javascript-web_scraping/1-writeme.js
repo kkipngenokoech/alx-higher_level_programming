e83827a8b7a3893c32d26299687a77560fff9100
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-12 03:50:32 -0700

    if error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs')
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error, data){
-	console.log( error || data)
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error){
+	if (error)
+		console.log( error)
 })
