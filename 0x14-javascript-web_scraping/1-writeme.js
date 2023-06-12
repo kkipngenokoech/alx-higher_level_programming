7cf01fccd61e867737526981baa8db28ef09356d
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-12 03:53:53 -0700

    refactor for checkers sake

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error) {
-	if (error)
-		console.log(error);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
