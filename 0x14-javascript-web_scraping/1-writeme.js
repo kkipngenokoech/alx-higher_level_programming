c3dbc92ae6aa67e47c297112832cd07fa1394fc3
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-12 03:52:50 -0700

    if checkers

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error) {
 	if (error)
-		console.log(error)
-})
+		console.log(error);
+});
