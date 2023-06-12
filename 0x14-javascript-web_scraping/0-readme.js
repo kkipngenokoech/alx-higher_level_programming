72d6a6cd3a0a595c30bd92224479aeaa41c8e4cb
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-12 03:43:11 -0700

    refactor

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs  = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, data) {
-	console.log(error || data);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
