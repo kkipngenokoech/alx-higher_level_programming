5eee9ca4a4f9d608e095cfb3747112d6b845806a
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-12 01:46:53 -0700

    double and rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+	constructor (w, h){
+		if ((w > 0) && (h > 0)){
+			this.width = w
+			this.height = h
+		}
+	}
+	print(){
+		for (let index = 0; index < this.height; index++)
+			console.log('X'.repeat(this.width))
+	}
+	rotate(){
+		let temp = this.width
+		this.width = this.height
+		this.height = temp
+	}
+	double(){
+		this.width *= 2
+		this.height *= 2
+	}
+}
+module.exports = Rectangle
