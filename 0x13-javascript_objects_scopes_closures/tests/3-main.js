6bb3db6129c6b2bd7f1fce62773a52fc8f9dbc6b
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-12 01:37:14 -0700

    this repeat() is working

--- 0x13-javascript_objects_scopes_closures/tests/3-main.js
+++ 0x13-javascript_objects_scopes_closures/tests/3-main.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-import sys
-sys.path.append('../')
+const Rectangle = require('../3-rectangle');
+
+const r1 = new Rectangle(2, 3);
+r1.print();
+
+const r2 = new Rectangle(10, 5);
+r2.print();
