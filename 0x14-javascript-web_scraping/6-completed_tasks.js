ade708909c57a56dbdbb03c0f26d9eac36c7649b
kipngeno koech <kkipngenokoech22@gmail.com>
2023-06-12 23:08:16 -0700

    success

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,26 @@
 #!/usr/bin/node
-const request = require('request')
-request(process.argv[2], (error, response, body) => {
-	if (error) {
-		console.log(error)
-	}
-	else {
-		const responseJSON = JSON.parse(body)
-		const completed = {}
-		responseJSON.forEach((task) => {
-			if (task.completed === true)
-				completed[task.userId]? completed[task.userId]++ : completed[task.userId] = 1
-		})
-		console.log(completed)
-	}
-})
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
